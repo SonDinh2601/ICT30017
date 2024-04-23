b80dd69c1785b9808e30c07d7a568ff4350afebb
103170333 <sondnn.ccc@gmail.com>
2024-04-23 12:26:09 +1000

    first commit

---
+++ components/header.inc.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home Page</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <link href="style.css" rel="stylesheet">
+
+</head>
+<body>
+
+<!-- NavBar Start -->
+<nav class="navbar navbar-expand-lg fixed-top">
+    <div class="container-fluid">
+        <!-- Logo -->
+        <a class="navbar-brand me-auto" href="index.php">
+            <img class="logo rounded" src="pic/main_logo.jpg" alt="Main Logo" width="50" height="50" >
+            Health Care Service
+        </a>
+
+        <!-- Off canvas start -->
+        <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+
+            <!-- Offcanvas Body -->
+            <div class="offcanvas-body">
+                <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Staff Management</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Service Management</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Client Management</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Facility Management</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <a href="#" class="login-btn">Login</a>
+
+        <!-- Navbar toggle button -->
+        <button class="navbar-toggler pe-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+</nav>
+<!-- NavBar End -->
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include('components/header.inc.php'); ?>
+    <!-- Hero Section Start-->
+    <section class="hero-section">
+        </section>
+    <!-- Hero Section End -->
+<?php include('components/footer.inc.php'); ?>
+
