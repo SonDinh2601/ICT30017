3307d58b65f16abc23dc22976748cd6ca4fb1cb6
SonDinh2601 <103170333@student.swin.edu.au>
2024-05-08 11:48:23 +1000

    commit

--- components/header-nav.inc.php
+++ components/header-nav.inc.php
@@ -3,13 +3,12 @@
     <div class="container-fluid">
         <!-- Logo -->
         <a class="navbar-brand me-auto" href="index.php">
-            <img class="logo rounded" src="pic/main_logo.jpg" alt="Main Logo" width="50" height="50">
+            <img class="logo rounded" src="pic/main_logo.jpg" alt="Main Logo" width="50" height="50" >
             Health Care Service
         </a>
 
         <!-- Off canvas start -->
-        <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar"
-            aria-labelledby="offcanvasNavbarLabel">
+        <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
             <div class="offcanvas-header">
                 <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
@@ -22,16 +21,16 @@
                         <a class="nav-link" href="index.php">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="services.php">Service Management</a>
+                        <a class="nav-link" href="#">Staff Management</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="facilities.php">Facility Management</a>
+                        <a class="nav-link" href="#">Service Management</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="staff_list.php">Staff Management</a>
+                        <a class="nav-link" href="#">Client Management</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="client_list.php">Client Management</a>
+                        <a class="nav-link" href="#">Facility Management</a>
                     </li>
                 </ul>
             </div>
@@ -39,8 +38,7 @@
         <a href="#" class="login-btn">Login</a>
 
         <!-- Navbar toggle button -->
-        <button class="navbar-toggler pe-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
-            aria-controls="offcanvasNavbar">
+        <button class="navbar-toggler pe-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
             <span class="navbar-toggler-icon"></span>
         </button>
     </div>
--- templates/head.php
+++ templates/head.php
@@ -1,6 +1,6 @@
 <head>
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
     <link href="style.css" rel="stylesheet">
 </head>
